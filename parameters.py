2f3ff15e42d507ecc524210a3f8845573985e634
silhouette <953928832@qq.com>
2024-08-14 15:39:10 +0800

    add new messages

--- parameters.py
+++ parameters.py
@@ -15,11 +15,11 @@ def get_params(argv='1'):
 
         # INPUT PATH
         # dataset_dir='DCASE2020_SELD_dataset/',  # Base folder containing the foa/mic and metadata folders
-        dataset_dir='../Dataset/STARSS2023',  # server '../../dataset/STARSS2023' 
+        dataset_dir='../../dataset/STARSS2023',  # server '../../dataset/STARSS2023' 
 
         # OUTPUT PATHS
         # feat_label_dir='DCASE2020_SELD_dataset/feat_label_hnet/',  # Directory to dump extracted features and labels
-        feat_label_dir='../Dataset/STARSS2023/feat_label_hnet/', # server '../../dataset/STARSS2023/feat_label_hnet/',
+        feat_label_dir='../../dataset/STARSS2023/feat_label_hnet/', # server '../../dataset/STARSS2023/feat_label_hnet/',
 
         model_dir='models',  # Dumps the trained models and training curves in this folder
         dcase_output_dir='results',  # recording-wise results are dumped in this path.
